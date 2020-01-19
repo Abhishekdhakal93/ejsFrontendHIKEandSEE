5405f83b657caac3ea6c55a930f423837d62a09d
dukestabhi <new.abhishekh@gmail.com>
2020-01-19 19:24:03 +0545

    frontend

---
+++ app.js
@@ -0,0 +1,36 @@
+var express =require('express');
+var app = express();
+var path = require('path')
+
+app.set('view engine','ejs');
+app.set('views','views');
+
+app.get('/',(req,res)=>{
+    res.render('index')
+});
+
+app.get('/login',(req,res)=>
+{
+    res.render('login')
+});
+
+app.get('/register',(req,res)=>
+{
+    res.render('register')
+});
+
+app.get('/home',(req,res)=>
+{
+    res.render('home')
+});
+
+app.get('/footer',(req,res)=>
+{
+    res.render('footer')
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(8000, (req, res) => {
+    console.log('server is running in port', +8000);
+});
